60d948e3fedcaf41c1f61a01f812245f689d9a4a
Omkar Chandorkar <gotenksIN@aospa.co>
2023-08-29 14:38:18 +0530

    munch: overlay: Enable secure NFC functionality
    Change-Id: I4e9acc7d8ec14237512aadf100d564984aa5a896
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: madmax7896 <madmax78968@gmail.com>

--- device.mk
+++ device.mk
@@ -366,10 +366,15 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
+# NFC
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.boot.hardware.sku=munch
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-aosp
+
 
 PRODUCT_PACKAGES += \
     MunchCNSettingsProviderOverlay \
@@ -377,7 +382,8 @@ PRODUCT_PACKAGES += \
     MunchGLOBALSettingsProviderOverlay \
     MunchGLOBALWifiOverlay \
     MunchINDIASettingsProviderOverlay \
-    MunchINDIAWifiOverlay
+    MunchINDIAWifiOverlay \
+    MunchNfcOverlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
