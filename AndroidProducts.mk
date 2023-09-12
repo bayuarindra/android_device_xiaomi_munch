0508ea7e4f5176fed615f94b4bf09817be5800d4
bayuarindra <bayuarindra2@gmail.com>
2023-09-12 19:46:14 +0700

    munch: Its Rising Time!
    Signed-off-by: bayuarindra <bayuarindra2@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/cherish_munch.mk
+    $(LOCAL_DIR)/lineage_munch.mk
 
 COMMON_LUNCH_CHOICES := \
-    cherish_munch-user \
-    cherish_munch-userdebug \
-    cherish_munch-eng
+    lineage_munch-user \
+    lineage_munch-userdebug \
+    lineage_munch-eng
