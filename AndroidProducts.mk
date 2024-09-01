2fd204ddfb88af767ceca7b8ad1849ce34fd3691
bayuarindra <bayuarindra2@gmail.com>
2024-09-01 10:47:48 +0700

    munch: init RisingOS
    Signed-off-by: bayuarindra <bayuarindra2@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/pixelstar_munch.mk
+    $(LOCAL_DIR)/lineage_munch.mk
 
 COMMON_LUNCH_CHOICES := \
-    pixelstar_munch-user \
-    pixelstar_munch-userdebug \
-    pixelstar_munch-eng
+    lineage_munch-user \
+    lineage_munch-userdebug \
+    lineage_munch-eng
